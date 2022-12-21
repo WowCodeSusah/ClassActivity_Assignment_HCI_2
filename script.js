e0a9cbc2bf56259da42317d28c1754baa42e78c9
WowCodeSusah <114371708+WowCodeSusah@users.noreply.github.com>
2022-12-21 09:23:05 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 // Add your answer here
+$('#1').hover(
+    function(){
+        $('#5').fadeIn("slow")
+    },
+    function(){
+        $('#5').fadeOut("slow")
+    }
+)
+
+$('#2').hover(
+    function(){
+        $('#6').fadeIn("slow")
+    },
+    function(){
+        $('#6').fadeOut("slow")
+    }
+)
+
+$('#3').hover(
+    function(){
+        $('#7').fadeIn("slow")
+    },
+    function(){
+        $('#7').fadeOut("slow")
+    }
+)
+
+$('#4').hover(
+    function(){
+        $('#8').fadeIn("slow")
+    },
+    function(){
+        $('#8').fadeOut("slow")
+    }
+)
